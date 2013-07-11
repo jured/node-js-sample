23c4342668c43b1284f6a0ec940f988d77885a73
jured <julc.93@gmail.com>
2013-07-11 16:42:13 +0000

    web.js rev3

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
-response.send(text);
+response.send("text");
 });
 
 var port = process.env.PORT || 5000;
